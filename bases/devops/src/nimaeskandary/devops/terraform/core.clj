48c68fd85edfe004187cee17db4926937dd37ca7
Nima Eskandary <nimaesk1@gmail.com>
2024-03-14 21:28:40 -0400

    hit localstack with cdktf

--- bases/devops/src/nimaeskandary/devops/terraform/core.clj
+++ bases/devops/src/nimaeskandary/devops/terraform/core.clj
@@ -1,30 +1,79 @@
 (ns nimaeskandary.devops.terraform.core
   (:import (com.hashicorp.cdktf App LocalBackend$Builder TerraformStack)
-           (com.hashicorp.cdktf.providers.aws.provider AwsProvider$Builder)
+           (com.hashicorp.cdktf.providers.aws.provider
+            AwsProvider$Builder
+            AwsProviderEndpoints$Builder)
            (com.hashicorp.cdktf.providers.aws.s3_bucket S3Bucket$Builder)
-           (com.hashicorp.cdktf.providers.aws.vpc Vpc$Builder)))
+           (com.hashicorp.cdktf.providers.aws.vpc Vpc$Builder)
+           (java.util ArrayList)))
 
-(def app (App.))
+(defn create-app [] (App.))
 
-(def stack (TerraformStack. app "application-stack"))
+(defn create-stack [scope] (TerraformStack. scope "application-stack"))
 
-(def backend
-  (-> (LocalBackend$Builder/create stack)
+(defn create-backend
+  [scope]
+  (-> (LocalBackend$Builder/create scope)
       (.path "application-stack.tfstate")
       (.workspaceDir "terraform")
       (.build)))
 
-(def provider
-  (-> (AwsProvider$Builder/create stack "AWS")
-      (.region "us-east-1")
+(defn create-provider
+  [scope region]
+  ;; https://docs.localstack.cloud/user-guide/integrations/terraform/
+  (let [localstack-endpoint "http://localhost:4566"]
+    (-> (AwsProvider$Builder/create scope "AWS")
+        (.region region)
+        (.profile "dev")
+        (.s3UsePathStyle true)
+        (.skipCredentialsValidation true)
+        (.skipMetadataApiCheck "true")
+        (.skipRequestingAccountId true)
+        (.endpoints
+         (ArrayList.
+          [(-> (AwsProviderEndpoints$Builder.)
+               (.apigateway localstack-endpoint)
+               (.apigatewayv2 localstack-endpoint)
+               (.cloudformation localstack-endpoint)
+               (.cloudwatch localstack-endpoint)
+               (.dynamodb localstack-endpoint)
+               (.ec2 localstack-endpoint)
+               (.es localstack-endpoint)
+               (.elasticache localstack-endpoint)
+               (.firehose localstack-endpoint)
+               (.iam localstack-endpoint)
+               (.kinesis localstack-endpoint)
+               (.lambda localstack-endpoint)
+               (.rds localstack-endpoint)
+               (.redshift localstack-endpoint)
+               (.route53 localstack-endpoint)
+               (.s3 localstack-endpoint)
+               (.secretsmanager localstack-endpoint)
+               (.ses localstack-endpoint)
+               (.sns localstack-endpoint)
+               (.sqs localstack-endpoint)
+               (.ssm localstack-endpoint)
+               (.stepfunctions localstack-endpoint)
+               (.sts localstack-endpoint)
+               (.build))]))
+        (.build))))
+
+(defn create-bucket
+  [scope]
+  (-> (S3Bucket$Builder/create scope "bucket")
       (.build)))
 
-(def bucket
-  (-> (S3Bucket$Builder/create stack "bucket")
+(defn create-vpc
+  [scope]
+  (-> (Vpc$Builder/create scope "private-vpc")
       (.build)))
 
-(def vpc
-  (-> (Vpc$Builder/create stack "private-vpc")
-      (.build)))
-
-(defn synth [_] (.synth app))
+(defn synth
+  [_]
+  (let [app (create-app)
+        application-stack (create-stack app)]
+    (create-backend application-stack)
+    (create-provider application-stack "us-east-1")
+    (create-bucket application-stack)
+    (create-vpc application-stack)
+    (.synth app)))
